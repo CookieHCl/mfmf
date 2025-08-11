007ca6c24632ede80048aa1a85022e3d1680f88f
CookieHCl <nhc7502@snu.ac.kr>
2025-08-11 21:36:22 +0900

    Add skeleton code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function hello(name: string): string {
-  return `Hello, ${name}!`;
+import * as matter from 'gray-matter';
+
+type Frontmatter = Record<string, any>;
+type Instruction = (oldFrontmatter: Frontmatter) => Frontmatter;
+
+export function transformFrontmatter(oldFrontmatter: Frontmatter, instruction: Instruction): Frontmatter {
+  return instruction(oldFrontmatter);
+}
+
+export function transformMarkdownFile(filepath: string, instruction: Instruction) {
+  const file = matter.read(filepath);
+
+  const oldFrontmatter = file.data;
+  const newFrontmatter = transformFrontmatter(structuredClone(oldFrontmatter), instruction);
+  file.data = newFrontmatter;
+  console.log('[transformMarkdown]', { language: file.language, oldFrontmatter, newFrontmatter });
+
+  const newfile = file.stringify(file.language);
+  console.log('[transformMarkdown]', newfile);
 }
