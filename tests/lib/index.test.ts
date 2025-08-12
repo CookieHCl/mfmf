9b235bd9b1333277348d70992bc58e9b73838025
CookieHCl <nhc7502@snu.ac.kr>
2025-08-12 16:09:18 +0900

    Refactor transformFrontmatter test into describe block

--- tests/lib/index.test.ts
+++ tests/lib/index.test.ts
@@ -1,42 +1,43 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { transformFrontmatter } from '../../src/index.js'
 
-// transformFrontmatter
-test('transform frontmatter', () => {
-  const oldFrontmatter = {
-    title: 'Test Title',
-    categories: ['PAPER', 'AI'],
-    tags: ['LLM', 'Google']
-  }
+describe('transformFrontmatter', () => {
+  test('transform frontmatter', () => {
+    const oldFrontmatter = {
+      title: 'Test Title',
+      categories: ['PAPER', 'AI'],
+      tags: ['LLM', 'Google']
+    }
 
-  const newFrontmatter = transformFrontmatter(oldFrontmatter, (old) => {
-    old.title = 'Updated Title'
-    old.date = '2023-01-01'
-    delete old.categories
-    old.tags.push('2017')
-    return old;
+    const newFrontmatter = transformFrontmatter(oldFrontmatter, (old) => {
+      old.title = 'Updated Title'
+      old.date = '2023-01-01 12:00:00'
+      delete old.categories
+      old.tags.push('2017')
+      return old;
+    })
+
+    expect(newFrontmatter).toEqual({
+      title: 'Updated Title',
+      tags: ['LLM', 'Google', '2017'],
+      date: '2023-01-01 12:00:00'
+    })
   })
 
-  expect(newFrontmatter).toEqual({
-    title: 'Updated Title',
-    date: '2023-01-01',
-    tags: ['LLM', 'Google', '2017']
-  })
-})
+  test('replace frontmatter', () => {
+    const oldFrontmatter = {
+      title: 'Test Title'
+    }
 
-test('replace frontmatter', () => {
-  const oldFrontmatter = {
-    title: 'Test Title'
-  }
+    const newFrontmatter = transformFrontmatter(oldFrontmatter, (old) => {
+      return {
+        categories: ["BLOG", "FUWARI"]
+      };
+    })
 
-  const newFrontmatter = transformFrontmatter(oldFrontmatter, (old) => {
-    return {
-      category: "BLOG"
-    };
-  })
-
-  expect(newFrontmatter).toEqual({
-    category: "BLOG"
+    expect(newFrontmatter).toEqual({
+      categories: ["BLOG", "FUWARI"]
+    })
   })
 })
 
